014d9d7cda22a3cf7e8e2d2c1b9c09af73c78ad1
Kapustin Anton <kapustin.anton199712@gmail.com>
2019-03-13 22:22:19 +0700

    Начальный вариант

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+            string buf;
+            for (int i=0; i<size; i++)
+            {
+                Console.WriteLine("Введите arr[{0}]", i);
+                buf = Console.ReadLine();
+                arr[i] = Convert.ToInt32(buf);
+            }
+            int d=1;
+            foreach(int x in arr)
+            {
+                if (x > 0) d = d * x;
+            }
+            int min = arr[0];
+            foreach (int x in arr)
+            {
+                if (x < min) min=x;
+            }
+            int sym = 0;
+            foreach (int x in arr)
+            {
+                if (x > min) sym=sym+x;
+                else break;
+            }
+            Console.WriteLine("Произведение положительных ненулевых элементов массива =" + d);
+            Console.WriteLine("Cумму элементов массива, расположенных до минимального элемента ({0}) = {1}" , min , sym);
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
