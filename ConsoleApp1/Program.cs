eadcaa5bd60361f5a20974650f4810c5ab6f21a1
Kapustin Anton <kapustin.anton199712@gmail.com>
2019-03-14 09:57:19 +0700

    Сортировка чет нечт отдельно, через массивы

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,34 +12,105 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Введите размер массива");
             int size = int.Parse(Console.ReadLine());
-            int[] arr = new int[size];
-            string buf;
-            for (int i=0; i<size; i++)
+            int g, g1;
+            if (size % 2 == 0)
             {
-                Console.WriteLine("Введите arr[{0}]", i);
-                buf = Console.ReadLine();
-                arr[i] = Convert.ToInt32(buf);
+                g = size / 2;
+                g1 = size / 2;
             }
-            int d=1;
-            foreach(int x in arr)
+            else
+            {
+                g = size / 2+1;
+                g1 = size / 2;
+            }
+            double[] arr = new double[size];
+            double[] arr1 = new double[g];
+            double[] arr2 = new double[g1];
+            string buf;
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Введите элемент массива {0}", i );
+                buf = Console.ReadLine();
+                arr[i] = Convert.ToDouble(buf);
+            }
+            double d = 1;
+            foreach (double x in arr)
             {
                 if (x > 0) d = d * x;
             }
-            int min = arr[0];
-            foreach (int x in arr)
+            double min = arr[0];
+            foreach (double x in arr)
             {
-                if (x < min) min=x;
+                if (x < min) min = x;
             }
-            int sym = 0;
-            foreach (int x in arr)
+            double sym = 0;
+            foreach (double x in arr)
             {
-                if (x > min) sym=sym+x;
+                if (x > min) sym = sym + x;
                 else break;
             }
+            int j = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    arr1[j] = arr[i];
+                    j++;
+                }
+            }
+            Array.Sort(arr1);
+            j = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    arr2[j] = arr[i];
+                    j++;
+                }
+            }
+            Array.Sort(arr2);
+            PrintArray("отсортированные четные элементы ", arr1);
+            PrintArray("отсортированные нечетные элементы", arr2);
+            j = 0;
+            if (size % 2 == 0)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    arr[i] = arr1[j];
+                    i++;
+                    arr[i] = arr2[j];
+                    j++;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < size-1; i++)
+                {
+                    arr[i] = arr1[j];
+                    i++;
+                    arr[i] = arr2[j];
+                    j++;
+                }
+                arr[size - 1] = arr1[j];
+            }
+            PrintArray("отсортированный массив", arr);
             Console.WriteLine("Произведение положительных ненулевых элементов массива =" + d);
-            Console.WriteLine("Cумму элементов массива, расположенных до минимального элемента ({0}) = {1}" , min , sym);
+            Console.WriteLine("Cумму элементов массива, расположенных до минимального элемента ({0}) = {1}", min, sym);
             Console.ReadKey();
             Console.ReadKey();
+        }
+        public static void PrintArray(string mes, double[] a)
+
+        {
+
+            Console.WriteLine(mes);
+
+            for (int i = 0; i < a.Length; i++)
+
+                Console.Write("\t" + a[i]);
+
+            Console.WriteLine();
+
         }
     }
 }
